25611b9a5a292df3b80f064c4ac5faba62f9a103
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-08 13:33:20 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Event =require('./models/event');
+
+
+require("dotenv").config();
+
+const app = express();
+
+
+const uri =process.env.CONNECTIONSTRING; 
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology:true})
+ .then((result) => {console.log('connected to db'); app.listen(process.env.PORT || 3000);})
+ .catch((err) => console.log(err));
+
+app.use(express.static('public'));
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
+
+
+var eventRoutes = require("./routes/eventRoutes");
+app.use(eventRoutes);
---
+++ routes/eventRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Event =require('../models/event');
+var bodyParser = require('body-parser')
+const route = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+-
+
+
+route.post('/addEvent', jsonParser,(req,res) => {
+    
+    const event = new Event({
+        name = req.body.name,
+        email = req.body.email,
+        phone = req.body.phone,
+        date = req.body.date,
+        time = req.body.time,
+        people = req.body.people,
+        type = req.body.type,
+        location = req.body.location,
+        hear = req.body.here, 
+        comments = req.body.comments
+    });
+
+    event.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => 
+        console.log(err));
+})
+
+
+
+module.exports = route;
