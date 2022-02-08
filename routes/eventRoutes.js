877c4411e8ce670da5515b2fc39357aa59507b4a
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-08 13:40:52 -0600

    fixed object initialization

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -15,16 +15,16 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 route.post('/addEvent', jsonParser,(req,res) => {
     
     const event = new Event({
-        name = req.body.name,
-        email = req.body.email,
-        phone = req.body.phone,
-        date = req.body.date,
-        time = req.body.time,
-        people = req.body.people,
-        type = req.body.type,
-        location = req.body.location,
-        hear = req.body.here, 
-        comments = req.body.comments
+        name : req.body.name,
+        email : req.body.email,
+        phone : req.body.phone,
+        date : req.body.date,
+        time : req.body.time,
+        people : req.body.people,
+        type : req.body.type,
+        location : req.body.location,
+        hear : req.body.here, 
+        comments : req.body.comments
     });
 
     event.save()
