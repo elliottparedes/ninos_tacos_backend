ddc535d0998a0c9d3f61edc08fb9a615b3ce1326
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-08 14:22:10 -0600

    changed cors to *

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://127.0.0.1:5500/', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
